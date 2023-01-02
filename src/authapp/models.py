b5352b5bdcb4e3337722215e9afaa659023cdb1a
mykhailokukol <mykhailokukol@gmail.com>
2023-01-02 15:52:13 +0200

    added User model; added reqs

---
+++ src/authapp/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    """ Custom User Manager """
+    
+    def create_user(self, email=None, password=None, **extra_fields):
+        if not email or not password:
+            raise ValueError('email and password field must be set')
+        extra_fields.setdefault('is_active', True)
+        email = self.normalize_email(email)
+        user = self.model(email=email, password=password, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+    """ User Model """
+    
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects = UserManager()
+    
+    email = models.EmailField(unique=True, db_index=True)
+    username = models.CharField(max_length=64, unique=True, db_index=True)
+    
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_online = models.BooleanField(default=False)
+    
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    date_updated = models.DateTimeField(auto_now=True, null=True)
+    
+    def __str__(self):
+        return f'{self.id} {self.username if self.username else self.email}'
--- src/src/settings.py
+++ src/src/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'chatapp',
+    'authapp',
     'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
