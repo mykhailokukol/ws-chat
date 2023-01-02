43c4c52457d90367ba08c9ca4000b30a06e2ee19
mykhailokukol <mykhailokukol@gmail.com>
2023-01-02 16:46:10 +0200

    added part of auth views

---
+++ src/authapp/serializers.py
@@ -0,0 +1,59 @@
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.hashers import make_password
+
+from rest_framework import serializers
+
+from authapp import models
+
+
+class LoginUserSerializer(serializers.Serializer):
+    """ Authentication SignIn Serializer """
+    
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True)
+    
+    def get_user(self, is_active=False):
+        if 'email' in self.data and 'password' in self.data:
+            try:
+                user = models.User.objects.get(
+                    is_active=is_active,
+                    email=self.data.get('email')
+                )
+                if user.has_usable_password():
+                    return user
+            except models.User.DoesNotExist:
+                pass
+            
+class SignUpUserSerializer(serializers.ModelSerializer):
+    """ Authentication SignUp Serializer """
+    
+    class Meta:
+        model = models.User
+        fields = (
+            'id', 'email', 'username', 'password', 
+        )
+        read_only_fields = ('id', )
+        extra_kwargs = {
+            'email': {'required': True},
+            'username': {'required': True},
+            'password': {'write_only': True}
+        }
+    
+    def validate(self, data):
+        validate_password(password=data['password'], user=models.User)
+        return data
+        
+    def create(self, validated_data):
+        if 'email' in validated_data and 'username' in validated_data:
+            validated_data['password'] = make_password(validated_data['password'])
+            return super(SignUpUserSerializer, self).create(validated_data)
+        else:
+            raise serializers.ValidationError('Unable to create user without email or username')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """ Default User Serializer """
+    
+    class Meta:
+        model = models.User
+        fields = '__all__'
--- src/authapp/views.py
+++ src/authapp/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
-# Create your views here.
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from authapp import models, serializers
+
+
+class LoginUserApi(viewsets.ModelViewSet):
+    """ Authentication viewset """
+    
+    queryset = models.User.objects.all()
+    serializer_class = serializers.LoginUserSerializer
+    
+    @action(['post'], detail=False)
+    def post(self, request):
+        """ SignIn User endpoint """
+        
+        self.serializer_class.is_valid(raise_exception=True)
+        data = self.serializer_class.validated_data
+        
+        user = models.User.objects.filter(email=data['email'])
+        if not user.exists():
+            return Response({'error': 'Email or password is invalid'}, status.HTTP_400_BAD_REQUEST)
+        user = user.last()
+        
+        if not user.is_active:
+            return Response({'error': 'Your account is inactive'}, status.HTTP_400_BAD_REQUEST)
+        
+        check_password = user.check_password(data['password'])
+        if check_password:
+            refresh = RefreshToken.for_user(user)
+            
+            return Response({
+                'access': str(refresh.access_token),
+                'refresh': str(refresh),
+            }, status.HTTP_201_CREATED)
+        return Response({'error': 'Email or password is invalid'}, status.HTTP_400_BAD_REQUEST)
+        
